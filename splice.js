c60eb51876065f5f4b7cc4baf471dc69be16ffdd
akshit <akkshetshan@gmail.com>
2023-07-24 18:08:56 +0530

    final comit

---
+++ splice.js
@@ -0,0 +1,5 @@
+const arr=["jan","feb","march","april","may","jun","july"];
+
+
+arr.splice(2,1,"mar");
+console.log(arr);
---
+++ string.js
@@ -0,0 +1,28 @@
+// // const lorum="Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+// // const text=lorum.slice(0,280-lorum.length);
+// // console.log(text);
+// // console.log(lorum.indexOf("vulputate"));
+
+
+// let text="HELLO WORLD";
+// console.log(text.charCodeAt(text.length-1));
+
+// // .charAt(text.length-1)claer
+
+
+let name=1;
+
+switch (name) {
+    case 0:
+        console.log("hello aanchal");
+        break;
+    case 1:
+        console.log("love you aanchal");
+        break;
+    case 2:
+        console.log("Miss you aanchal");
+        break;
+    default:
+        console.log("nothing");
+        break;
+}
